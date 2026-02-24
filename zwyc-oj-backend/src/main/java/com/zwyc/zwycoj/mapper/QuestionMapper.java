0ab9da40f95f47ef1f2f3c33b357c89d9ae56f96
Acw <astrolithia@gmail.com>
2026-02-24 16:11:18 +0800

    后端基本接口开发 前端基础页面搭建

---
+++ zwyc-oj-backend/src/main/java/com/zwyc/zwycoj/mapper/QuestionMapper.java
@@ -0,0 +1,18 @@
+package com.zwyc.zwycoj.mapper;
+
+import com.zwyc.zwycoj.model.entity.Question;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+* @author shiro
+* @description 针对表【question(题目)】的数据库操作Mapper
+* @createDate 2026-02-16 10:37:47
+* @Entity com.zwyc.zwycoj.model.entity.Question
+*/
+public interface QuestionMapper extends BaseMapper<Question> {
+
+}
+
+
+
+
---
+++ zwyc-oj-backend/src/main/java/com/zwyc/zwycoj/mapper/QuestionSubmitMapper.java
@@ -0,0 +1,18 @@
+package com.zwyc.zwycoj.mapper;
+
+import com.zwyc.zwycoj.model.entity.QuestionSubmit;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+* @author shiro
+* @description 针对表【question_submit(题目提交)】的数据库操作Mapper
+* @createDate 2026-02-16 10:40:13
+* @Entity com.zwyc.zwycoj.model.entity.QuestionSubmit
+*/
+public interface QuestionSubmitMapper extends BaseMapper<QuestionSubmit> {
+
+}
+
+
+
+
